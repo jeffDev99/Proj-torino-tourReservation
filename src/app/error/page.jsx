d13e652a2a528c9712472fec17d7038117054950
S.Mohammad.Amin.Jafarnezhad <mohammadaminjafarnezhad6@gmail.com>
2024-12-07 15:26:38 +0330

    404 and 500 page

---
+++ src/app/error/page.jsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import Container from "./components/templates/Container";
+import Image from "next/image";
+export default function ErrorPage({error}) {
+  return (
+    <Container>
+      <figure className="flex flex-col-reverse lg:flex-row mb-36 lg:mb-0">
+        <figcaption className="lg:w-1/2 flex flex-col justify-center">
+          <h1 className=" text-2xl lg:text-4xl text-gray mb-7">صفحه مورد نظر یافت نشد!</h1>
+          <p className="text-2xl">لطفا بعدا دوباره امتحان کنید.</p>
+        </figcaption>
+        <Image width={555} height={555} src="/Error Lamp Robot.svg" alt="error 404 page not found"  className="lg:w-1/2" />
+      </figure>
+    </Container>
+  );
+}
---
+++ src/app/not-found.jsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import Container from "./components/templates/Container";
+import Image from "next/image";
+export default function NotFoundPage() {
+  return (
+    <Container>
+      <figure className="flex flex-col-reverse lg:flex-row mb-36 lg:mb-0">
+        <figcaption className="lg:w-1/2 flex flex-col items-center justify-center">
+          <h1 className=" text-2xl lg:text-4xl text-gray mb-7">صفحه مورد نظر یافت نشد!</h1>
+          <Link href="/" className="bg-[#D8FFE1] text-primary rounded-2xl py-3 lg:py-4 px-5 lg:px-11 text-xl lg:text-[28px]">بازگشت به صفحه اصلی</Link>
+        </figcaption>
+        <Image width={555} height={555} src="/Error TV.svg" alt="error 404 page not found"  className="lg:w-1/2" />
+      </figure>
+    </Container>
+  );
+}
