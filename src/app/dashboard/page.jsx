14dd66e0b4f727eb14ffa46aef7f8b06dd353db7
S.Mohammad.Amin.Jafarnezhad <mohammadaminjafarnezhad6@gmail.com>
2024-12-14 16:39:49 +0330

    header bug fix and structure profile page

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import DashboardLayout from '@/components/templates/layout/DashboardLayout'
+export default function page() {
+  return (
+    <DashboardLayout>
+        <h1>Dashboard</h1>
+    </DashboardLayout>
+  )
+}
