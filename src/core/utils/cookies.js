c2af4d16856a7ffa69bc59a0948f41286322215f
S.Mohammad.Amin.Jafarnezhad <mohammadaminjafarnezhad6@gmail.com>
2024-12-14 13:57:19 +0330

    bug fix : deploy on vercel, change react to ver 18

--- src/core/utils/cookies.js
+++ src/core/utils/cookies.js
@@ -4,10 +4,7 @@ function setCookie(name, value, days = 30) {
   document.cookie = `${name}=${value}; max-age=${maxAge}; path=/`;
 }
 const getCookies = (cookieName) => {
-  return document.cookie
-    .split(";")
-    .find((token) => token.trim().split("=")[0] === cookieName)
-    ?.split("=")[1];
+  return document.cookie.split(";").find((token) => token.trim().split("=")[0] === cookieName)?.split("=")[1];
 };
 const deleteCookie = (cookieName) => {
   document.cookie = `${cookieName}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
